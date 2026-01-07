881020c40137a2ab2a00bad23827f0c82428acfa
Tofutush <xiexuanqisirocco@163.com>
2026-01-06 23:11:04 -0500

    grid

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,18 @@
 <script lang="ts">
+    let house = [];
+    for (let z = 0; z < 9; z++) {
+        house[z] = [{}, {}, {}, {}, {}];
+    }
 </script>
 
-<main></main>
+<main>
+    <section id="house">
+        {#each house as rank}
+            {#each rank as room}
+                <div class="room"></div>
+            {/each}
+        {/each}
+    </section>
+    <section id="outer"></section>
+    <section id="directory"></section>
+</main>
