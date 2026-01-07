d6516715d5c46301f300e2aa94dea2f1687b5dba
Tofutush <xiexuanqisirocco@163.com>
2026-01-07 12:27:41 -0500

    tabs

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-    let house = [];
-    for (let z = 0; z < 9; z++) {
-        house[z] = [{}, {}, {}, {}, {}];
-    }
+	let house = [];
+	for (let z = 0; z < 9; z++) {
+		house[z] = [{}, {}, {}, {}, {}];
+	}
 </script>
 
 <main>
-    <section id="house">
-        {#each house as rank}
-            {#each rank as room}
-                <div class="room"></div>
-            {/each}
-        {/each}
-    </section>
-    <section id="outer"></section>
-    <section id="directory"></section>
+	<section id="house">
+		{#each house as rank}
+			{#each rank as room}
+				<div class="room"></div>
+			{/each}
+		{/each}
+	</section>
+	<section id="outer"></section>
+	<section id="directory"></section>
 </main>
