548e6cd9705e8b037554fa7d9c57c1beb07b09f0
Tofutush <xiexuanqisirocco@163.com>
2026-01-08 16:29:14 -0500

    JHSASDSHFSJBDF

--- src/components/RoomCard.svelte
+++ src/components/RoomCard.svelte
@@ -7,15 +7,17 @@
 	let { room, direction, enabled = true }: componentProps = $props();
 
 	let roomInstance = $derived(new Room(room));
-	if (direction) roomInstance.direction = direction;
 
-	let style = $state('');
-	if (!enabled) style += 'filter: grayscale(1);';
-	if (roomInstance.direction === 'e') style += 'transform: rotate(90deg);';
-	else if (roomInstance.direction === 's') style += 'transform: rotate(180deg);';
-	else if (roomInstance.direction === 'w') style += 'transform: rotate(270deg);';
+	function magicButton() {
+		enabled = !enabled;
+	}
+	function turn() {
+		direction = 'e';
+	}
 </script>
 
 <div class="room">
-	<img alt={roomInstance.name} src={roomImgs[roomInstance.getImgName()]} style={enabled ? '' : 'filter: grayscale(1)'} />
+	<button onclick={magicButton}>toggle</button>
+	<button onclick={turn}>Turn</button>
+	<img alt={roomInstance.name} src={roomImgs[roomInstance.getImgName()]} class="{enabled ? '' : 'disabled'} dir-{direction}" />
 </div>
