3fc36fd0b0d7f4af857f755dbce190b9ca8c0ab7
Tofutush <xiexuanqisirocco@163.com>
2026-01-09 14:24:23 -0500

    roomdir

--- src/components/Directory.svelte
+++ src/components/Directory.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
 	import { getRoomImg, roomList } from '../functions';
+	import type { RoomDir } from '../types';
+
+	let directory: RoomDir[] = roomList.map((r) => {
+		return { room: r, direction: 'n', enabled: true };
+	});
 </script>
 
 <section id="directory">
 	<details class="dir-section">
 		<summary>Rooms 1 - 12</summary>
 		<div class="dir-rooms">
-			{#each roomList.slice(0, 12) as room}
-				<img src={getRoomImg(room, 'n')} alt={room.name} />
+			{#each directory.slice(0, 12) as room}
+				<img src={getRoomImg(room.room, 'n')} alt={room.room.name} />
 			{/each}
 		</div>
 	</details>
