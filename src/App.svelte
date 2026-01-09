c617547f488b693898e193df8212f128823f7851
Tofutush <xiexuanqisirocco@163.com>
2026-01-08 19:41:22 -0500

    start the fuck over

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,4 @@
 <script lang="ts">
-	import Room from './classes/Room';
-	import House from './components/House.svelte';
-	import RoomDirectory from './components/RoomDirectory.svelte';
-	let house: Room[][] = [];
-	for (let z = 0; z < 9; z++) {
-		house[z] = [new Room(), new Room(), new Room(), new Room(), new Room()];
-	}
-	house[0][2] = new Room({
-		name: 'Antechamber',
-		color: ['none'],
-		filename: 'antechamber',
-		doors: ['e', 'n', 's', 'w'],
-	});
-	house[8][2] = new Room({
-		name: 'Entrance Hall',
-		color: ['blue'],
-		filename: 'entrancehall',
-		doors: ['e', 'n', 'w'],
-	});
 </script>
 
-<main>
-	<House {house} />
-	<section id="outer"></section>
-	<RoomDirectory />
-</main>
+<main></main>
--- src/state.svelte.ts
+++
@@ -1,5 +0,0 @@
-class State {
-    isDrafting = $state(false);
-    draftingLocation = $state([0, 0]);
-}
-export default State;
