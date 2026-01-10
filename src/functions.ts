d635415e2f1796d027205722a84a4caadd6d7ec9
Tofutush <xiexuanqisirocco@163.com>
2026-01-10 13:46:59 -0500

    add rooms!

--- src/functions.ts
+++ src/functions.ts
@@ -8,14 +8,25 @@ let roomImgs = Object.fromEntries(Object.entries(import.meta.glob('./assets/room
 
 export const roomList: RoomData[] = roomListJson as RoomData[];
 
+const specialSticks = [
+	"spareroom-secret",
+	"lockerroom",
+	"secretpassage",
+	"ballroom",
+	"rumpusroom",
+	"draftingstudio",
+	"workshop"
+]
+
 export function getRoomImg(room: RoomData, direction: Direction) {
 	if (!room.filename) throw new Error('no filename');
 	// special rooms
 	if (room.name === 'Antechamber') return roomImgs['antechamber.png'];
 	if (room.name === 'Entrance Hall') return roomImgs['entrancehall-4.png'];
 	if (room.name === 'Room 46') return roomImgs['room46-n.png'];
+	if (room.name === 'Chamber of Mirrors' && direction === 'n') return roomImgs['chamberofmirrors-4.png'];
 	// stick-shaped rooms
-	if (room.doors.length === 2 && room.doors.includes('n') && room.doors.includes('s') && room.filename !== "spareroom-secret") {
+	if (room.doors.length === 2 && room.doors.includes('n') && room.doors.includes('s') && !specialSticks.includes(room.filename)) {
 		if (direction === 'n' || direction === 's') return roomImgs[room.filename + '-ns.png'];
 		else return roomImgs[room.filename + '-ew.png'];
 	}
