9426791b7d5325a5eeccec556a4cd7260b29176a
Tofutush <xiexuanqisirocco@163.com>
2026-01-10 14:10:16 -0500

    more info

--- src/components/Info.svelte
+++ src/components/Info.svelte
@@ -6,11 +6,13 @@
 </script>
 
 <section id="info">
-	<p>Hello! Welcome to the <a href="https://www.blueprincegame.com/">Blue Prince</a> House Builder where you can just. Put these pretty little room thumbnails where they belong.</p>
+	<p>Hello! Welcome to the <a target="_blank" href="https://www.blueprincegame.com/">Blue Prince</a> House Builder where you can just. Put these pretty little room thumbnails where they belong.</p>
 	<p>Not affiliated with the devs or publisher or anyone. Made for educational purposes.</p>
 	<p><strong>Spoilers for Blue Prince will be present, as I've listed every single room, found floorplan, studio addition, and upgrade in here!</strong></p>
 	<p>You can try not clicking into these categories though.</p>
-	<p>This is not very mobile-friendly. I recommend browsing on PC.</p>
+	<p>This is <string>not very mobile-friendly</string>. Though it's not unusable on mobile, I still recommend browsing on PC.</p>
+	<p>Images may take a while to load.</p>
+	<p>And… yes, you can delete the Antechamber and Entrance Hall. Rooms also have no drafting constraints other than no path going outside the house. So go wild. If you're stuck, <strong>refreshing will completely erase your progress</strong>.</p>
 	<p>If you're ready to proceed, click the button below.</p>
 	<button onclick={proceed}>Proceed</button>
 </section>
